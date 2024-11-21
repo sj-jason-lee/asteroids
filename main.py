3f40bb3779e2dd87aa2c751752b194b9e5b037bf
sj-jason-lee <jason.sj.lee@outlook.com>
2024-11-21 14:54:15 -0500

    fix formatting

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
       for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          return
-      
+          if event.type == pygame.QUIT:
+              return
+
       screen.fill("black")
       player.draw(screen)
       pygame.display.flip()
-      
-      # limit fps to 60
-      dt = clock.tick(60)
+
+      # limit the framerate to 60 FPS
+      dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
