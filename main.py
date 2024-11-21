797319a0a8a6712c46d0bbe0bea430cd5b94476d
sj-jason-lee <jason.sj.lee@outlook.com>
2024-11-21 14:51:57 -0500

    create player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -14,6 +15,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
       for event in pygame.event.get():
@@ -21,6 +23,7 @@ def main():
           return
       
       screen.fill("black")
+      player.draw(screen)
       pygame.display.flip()
       
       # limit fps to 60
